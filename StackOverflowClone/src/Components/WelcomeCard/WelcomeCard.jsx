3735ec037ea6c8ef1f7200346bc903303815282c
PhucMaii <maithienphuc0102@gmail.com>
2023-10-19 23:34:40 -0700

    Fix Home Page UI and Navbar

--- StackOverflowClone/src/Components/WelcomeCard/WelcomeCard.jsx
+++ StackOverflowClone/src/Components/WelcomeCard/WelcomeCard.jsx
@@ -2,11 +2,11 @@ import { Button } from 'primereact/button'
 import PropTypes from 'prop-types'
 import './style.css'
 
-export default function WelcomeCard({buttonText, buttonType, isOutlined, message}) {
+export default function WelcomeCard({buttonText, buttonType, handleButtonClick, isOutlined, message}) {
   return (
     <div className="welcome-card-container">
-        <h3>{message}</h3>
-        <Button outlined={isOutlined} severity={buttonType}>{buttonText}</Button>
+        <h2>{message}</h2>
+        <Button onClick={handleButtonClick} outlined={isOutlined} severity={buttonType}>{buttonText}</Button>
     </div>
   )
 }
@@ -14,6 +14,7 @@ export default function WelcomeCard({buttonText, buttonType, isOutlined, message
 WelcomeCard.propTypes = {
   buttonText: PropTypes.string.isRequired,
   buttonType: PropTypes.string.isRequired,
+  handleButtonClick: PropTypes.func.isRequired,
   isOutlined: PropTypes.bool,
   message: PropTypes.string.isRequired,
 }
